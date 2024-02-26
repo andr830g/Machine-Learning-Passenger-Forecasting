5a1ac412302c31312b89647628d64d57f934873a
Andreas Skiby Andersen <62239677+andr830g@users.noreply.github.com>
2024-02-26 12:55:25 +0100

    Added trend and stationarity

--- DataSplitter.py
+++ DataSplitter.py
@@ -7,6 +7,7 @@ def getTrainData(agglevel=60, train_test_split='2023-06-01', start_date='2021-06
     file = f'data{agglevel}.csv'
     df = pd.read_csv(path + file, sep=',')
     df = df.sort_values(by='datetime')
+    df = addFeatures(df)
     train = df[(df['date'] >= start_date) & (df['date'] < train_test_split)]
     return train
 
@@ -17,9 +18,14 @@ def getTestData(agglevel=60, train_test_split='2023-06-01', end_date='2023-12-31
     file = f'data{agglevel}.csv'
     df = pd.read_csv(path + file, sep=',')
     df = df.sort_values(by='datetime')
+    df = addFeatures(df)
     test = df[(df['date'] >= train_test_split) & (df['date'] <= end_date)]
     return test
 
 def performTrainValSplit(df, train_val_split=''):
     raise NotImplementedError
     #return df_train, df_val
+
+
+def addFeatures(df):
+    return df
