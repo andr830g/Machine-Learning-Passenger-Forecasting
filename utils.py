46ca4be01a0e6b1059b3236cd61a4316b9f2287f
Andreas Skiby Andersen <62239677+andr830g@users.noreply.github.com>
2024-03-19 13:47:08 +0100

    Added with and without lags for sklearn

--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@ import numpy as np
 import time
 from skforecast.ForecasterAutoreg import ForecasterAutoreg
 import pandas as pd
+from sktime.transformations.series.difference import Differencer
 
 def MAE(y, yhat):
     """
@@ -58,6 +59,139 @@ class Time:
         return f'Execution time: {self.endTime - self.startTime:.3f} sec'
 
 
+def formatFittedValues(y_pred, y_true):
+    # round and cut off
+    y_pred = y_pred.round()
+    y_pred[y_pred < 0] = 0
+
+    # re-index
+    y_pred.index = y_true.index
+    return y_pred
+
+
+def difference(y_pred):
+    y_pred_diff = y_pred.diff(periods=1).fillna(0)
+    return y_pred_diff
+
+
+def inverseDifferencing(y_pred_diff, y_true, horizon):
+    y_pred = pd.Series()
+    indexrange = range(y_true.index[0], y_true.index[-1] + 1, horizon)
+    for idx, time in enumerate(indexrange):
+        if idx == 0:
+            constant = y_true.loc[time]
+        else:
+            constant = y_true.loc[time-1]
+        preds = y_pred_diff.loc[time : time+horizon-1].cumsum() + constant
+        y_pred = pd.concat([y_pred, preds])
+    y_pred.index = y_true.index
+    return y_pred
+
+
+def getFittedValues(model, X_train_temp, y_train_diff, scalar, exog_scalar):
+    X_train_lagdiff_transformed = pd.DataFrame(exog_scalar.fit_transform(X_train_temp), columns=X_train_temp.columns)
+    y_train_transformed = scalar.fit_transform(y_train_diff.to_numpy().reshape(-1, 1))
+    model.fit(X_train_lagdiff_transformed, y_train_transformed)
+    y_train_pred_transformed = model.predict(X_train_lagdiff_transformed).reshape(-1, 1)
+    y_train_pred_diff = pd.Series(scalar.inverse_transform(y_train_pred_transformed).squeeze(1))
+    return y_train_pred_diff
+
+
+def fixedWindowWithLags(X_train, y_train, X_val, y_val, model, horizon, differentiation, lags, scalar, exog_scalar, useExog):
+    ######
+    # forecasted values, lags and differencing handled automatically
+    ######
+    forecaster = ForecasterAutoreg(
+        regressor           = model,
+        lags                = lags,
+        differentiation     = differentiation,
+        transformer_y       = scalar,            
+        transformer_exog    = exog_scalar
+    )
+
+    # fit forecaster
+    forecaster.fit(y=y_train, exog=X_train)
+
+    y_val_pred = pd.Series()
+    indexrange = range(y_val.index[0], y_val.index[-1] + 1, horizon)
+
+    # perform forecast
+    timer = Time()
+    timer.start()
+    for idx, time in enumerate(indexrange):
+        # include max_lag last obs in train because of lags
+        last_window = y_train.tail(forecaster.max_lag + 1)
+
+        # include validation obs from first index to step before forecast
+        if idx > 0:
+            last_window = pd.concat([last_window, y_val.loc[:time-1]])
+    
+        # forecast starting from last window
+        if useExog == True:
+            preds = forecaster.predict(steps=horizon, exog=X_val.loc[time:time+horizon], last_window=last_window)
+        else:
+            preds = forecaster.predict(steps=horizon, exog=None, last_window=last_window)
+        y_val_pred = pd.concat([y_val_pred, preds])
+    
+    print(timer.end())
+
+    #######
+    # fitted values
+    #######
+    # difference
+    if differentiation == 1:
+        y_train_diff = difference(y_train)
+    else:
+        y_train_diff = y_train.copy(deep=True)
+    
+    # add lagged values
+    if useExog == True:
+        X_train_temp = X_train.copy(deep=True)
+    else:
+        X_train_temp = pd.DataFrame(index=y_train.index)
+    
+    if lags:
+        for lag in lags:
+            X_train_temp[f'lag_{lag}'] = y_train_diff.shift(periods=lag).fillna(0)
+
+    # fit differenced and transformed
+    y_train_pred_diff = getFittedValues(model, X_train_temp, y_train_diff, scalar, exog_scalar)
+
+    # inverse difference
+    if differentiation == 1:
+        y_train_pred = inverseDifferencing(y_train_pred_diff, y_train, horizon)
+    else:
+        y_train_pred = y_train_pred_diff.copy(deep=True)
+
+    return y_train_pred, y_val_pred
+
+
+def fixedWindowWithoutLags(X_train, y_train, X_val, y_val, model, horizon, differentiation, scalar, exog_scalar):
+    # diff
+    if differentiation == 1:
+        y_train_diff = difference(y_train)
+    else:
+        y_train_diff = y_train.copy()
+
+    # fit differenced and transformed
+    y_train_pred_diff = getFittedValues(model, X_train, y_train_diff, scalar, exog_scalar)
+    
+    # predict
+    X_val_transformed = pd.DataFrame(exog_scalar.transform(X_val), columns=X_val.columns)
+    y_val_pred_transformed = model.predict(X_val_transformed).reshape(-1, 1)
+    y_val_pred_diff = pd.Series(scalar.inverse_transform(y_val_pred_transformed).squeeze(1))
+    y_val_pred_diff.index = y_val.index
+
+    # inverse diff
+    if differentiation == 1:
+        y_train_pred = inverseDifferencing(y_train_pred_diff, y_train, horizon)
+        y_val_pred = inverseDifferencing(y_val_pred_diff, y_val, horizon)
+    else:
+        y_train_pred = y_train_pred_diff.copy()
+        y_val_pred = y_val_pred_diff.copy()
+
+    return y_train_pred, y_val_pred
+
 
 def fixedWindowForecastSklearn(X_train, y_train, X_val, y_val, model, 
                                horizon, differentiation=None, lags=None, scalar=None, exog_scalar=None,
@@ -65,78 +199,21 @@ def fixedWindowForecastSklearn(X_train, y_train, X_val, y_val, model,
     useLags = False if lags is None else True
     useExog = False if X_train is None else True
     assert useLags == True or useExog == True, 'must use lags and/or exogenous variables'
-    if useLags == False and differentiation == 1:
-        raise NotImplementedError
 
-    # forecast validation with lags
     if useLags == True:
-        # initiate forecaster
-        forecaster = ForecasterAutoreg(
-            regressor           = model,
-            lags                = lags,
-            differentiation     = differentiation,
-            transformer_y       = scalar,
-            transformer_exog    = exog_scalar
-        )
-
-        # fit forecaster
-        forecaster.fit(y=y_train, exog=X_train)
-
-        y_val_pred = pd.Series()
-        indexrange = range(y_val.index[0], y_val.index[-1] + 1, horizon)
-
-        timer = Time()
-        timer.start()
-        for idx, time in enumerate(indexrange):
-            # include max_lag last obs in train because of lags
-            last_window = y_train.tail(forecaster.max_lag + 1)
-
-            # include validation obs from first index to step before forecast
-            if idx > 0:
-                last_window = pd.concat([last_window, y_val.loc[:time-1]])
-    
-            # forecast starting from last window
-            if useExog == True:
-                preds = forecaster.predict(steps=horizon, exog=X_val.loc[time:time+horizon], last_window=last_window)
-            else:
-                preds = forecaster.predict(steps=horizon, exog=None, last_window=last_window)
-            y_val_pred = pd.concat([y_val_pred, preds])
-        print(timer.end())
-
-    # fit model to get fitted training values
-    if useExog == True:
-        X_train_temp = X_train.copy(deep=True)
-        X_val_temp = X_val.copy(deep=True)
+        # with lags with or without exog
+        y_train_pred, y_val_pred = fixedWindowWithLags(X_train, y_train, X_val, y_val, model, 
+                                         horizon, differentiation, lags, 
+                                         scalar, exog_scalar, useExog)
     else:
-        X_train_temp = pd.DataFrame(index=y_train.index)
-        X_val_temp = pd.DataFrame(index=y_val.index)
-    if lags:
-        for lag in lags:
-            # find the lagged values of train and val
-            X_train_temp[f'lag_{lag}'] = y_train.shift(periods=lag).fillna(0)
-            X_val_temp[f'lag_{lag}'] = pd.concat([y_train, y_val]).shift(periods=lag).fillna(0)[y_val.index]
+        # without lags with exog
+        y_train_pred, y_val_pred = fixedWindowWithoutLags(X_train, y_train, X_val, y_val, model, 
+                                                          horizon, differentiation, scalar, exog_scalar)
 
-    X_train_temp_transformed = pd.DataFrame(exog_scalar.fit_transform(X_train_temp), columns=X_train_temp.columns)
-    y_train_temp_transformed = scalar.fit_transform(y_train.to_numpy().reshape(-1, 1))
-    model.fit(X_train_temp_transformed, y_train_temp_transformed)
-    y_train_pred_transformed = model.predict(X_train_temp_transformed).reshape(-1, 1)
-    y_train_pred = pd.Series(scalar.inverse_transform(y_train_pred_transformed).squeeze(1))
-
-    # forecast validation without lags
-    if useLags == False:
-        X_val_temp_transformed = pd.DataFrame(exog_scalar.fit_transform(X_val_temp), columns=X_val_temp.columns)
-        y_val_pred_transformed = model.predict(X_val_temp_transformed).reshape(-1, 1)
-        y_val_pred = pd.Series(scalar.inverse_transform(y_val_pred_transformed).squeeze(1))
-
-    # round and cut off
-    y_train_pred = y_train_pred.round()
-    y_train_pred[y_train_pred < 0] = 0
-    y_val_pred = y_val_pred.round()
-    y_val_pred[y_val_pred < 0] = 0
-
-    y_train_pred.index = y_train.index
-    y_val_pred.index = y_val.index
+    y_train_pred = formatFittedValues(y_train_pred, y_train)
+    y_val_pred = formatFittedValues(y_val_pred, y_val)
     return model, y_train_pred, y_val_pred
+
 
 
 def expandingWindowForecastSklearn(X_train, y_train, X_val, y_val, model, 
@@ -192,11 +269,8 @@ def expandingWindowForecastSklearn(X_train, y_train, X_val, y_val, model,
     y_train_pred_transformed = model.predict(X_train_temp_transformed).reshape(-1, 1)
     y_train_pred = pd.Series(scalar.inverse_transform(y_train_pred_transformed).squeeze(1))
 
-    # round and cut off
-    y_train_pred = y_train_pred.round()
-    y_train_pred[y_train_pred < 0] = 0
-    y_val_pred = y_val_pred.round()
-    y_val_pred[y_val_pred < 0] = 0
+    y_train_pred = formatFittedValues(y_train_pred, y_train)
+    y_val_pred = formatFittedValues(y_val_pred, y_val)
     return model, y_train_pred, y_val_pred
 
 
@@ -252,9 +326,6 @@ def slidingWindowForecastSklearn(X_train, y_train, X_val, y_val, model,
     y_train_pred_transformed = model.predict(X_train_temp_transformed).reshape(-1, 1)
     y_train_pred = pd.Series(scalar.inverse_transform(y_train_pred_transformed).squeeze(1))
 
-    # round and cut off train and val predictions
-    y_train_pred = y_train_pred.round()
-    y_train_pred[y_train_pred < 0] = 0
-    y_val_pred = y_val_pred.round()
-    y_val_pred[y_val_pred < 0] = 0
+    y_train_pred = formatFittedValues(y_train_pred, y_train)
+    y_val_pred = formatFittedValues(y_val_pred, y_val)
     return model, y_train_pred, y_val_pred
