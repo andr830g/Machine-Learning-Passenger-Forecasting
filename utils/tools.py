581dc7c690f575d65b68da524735aa7cad6a8ff8
Andreas Skiby Andersen <62239677+andr830g@users.noreply.github.com>
2024-03-22 19:22:11 +0100

    Added residuals to plot

--- utils/tools.py
+++ utils/tools.py
@@ -47,45 +47,71 @@ def plotFitAndPredictions(y_train_pred, y_val_pred, y_train_true, y_val_true, tr
     assert (len(trainDateCol) > 0 and len(valDateCol) > 0) or not dates, 'must input trainDateCol and valDateCol or else set dates=False'
 
     text_constant = 19
-    fig, ax = plt.subplots(1, 2, figsize=(12, 6))
+    fig, ax = plt.subplots(2, 2, figsize=(14, 12))
     fig.set_tight_layout('h_pad')
 
-    ax[0].plot(y_train_true.index, y_train_true, color='red', label='gt')
-    ax[0].plot(y_train_true.index, y_train_pred, color='blue', alpha=0.5, label='fitted')
-    ax[0].set_title('Fitted Training Data')
-    ax[0].legend(loc='upper right')
-    ax[0].set_xlim(y_train_true.index.start, y_train_true.index.stop)
-    ax[0].set_ylim(np.min(y_train_true), np.max(y_train_true) + np.max(y_train_true)//6)
-    ax[0].set_xlabel('Date')
-    ax[0].set_ylabel('Passenger Count')
+    ax[0, 0].plot(y_train_true.index, y_train_true, color='red', label='gt')
+    ax[0, 0].plot(y_train_true.index, y_train_pred, color='blue', alpha=0.5, label='fitted')
+    ax[0, 0].set_title('Fitted Training Data')
+    ax[0, 0].legend(loc='upper right')
+    ax[0, 0].set_xlim(y_train_true.index.start, y_train_true.index.stop)
+    ax[0, 0].set_ylim(np.min(y_train_true), np.max(y_train_true) + np.max(y_train_true)//6)
+    ax[0, 0].set_xlabel('Date')
+    ax[0, 0].set_ylabel('Passenger Count')
 
-    ax[1].plot(y_val_true.index, y_val_true, color='red', label='gt')
-    ax[1].plot(y_val_true.index, y_val_pred, color='blue', alpha=0.5, label='pred')
-    ax[1].set_title('Predicted Validation Data')
-    ax[1].legend(loc='upper right')
-    ax[1].set_xlim(y_val_true.index.start, y_val_true.index.stop)
-    ax[1].set_ylim(np.min(y_val_true), np.max(y_val_true) + np.max(y_val_true)//6)
-    ax[1].set_xlabel('Date')
-    ax[1].set_ylabel('Passenger Count')
+    ax[0, 1].plot(y_val_true.index, y_val_true, color='red', label='gt')
+    ax[0, 1].plot(y_val_true.index, y_val_pred, color='blue', alpha=0.5, label='pred')
+    ax[0, 1].set_title('Predicted Validation Data')
+    ax[0, 1].legend(loc='upper right')
+    ax[0, 1].set_xlim(y_val_true.index.start, y_val_true.index.stop)
+    ax[0, 1].set_ylim(np.min(y_val_true), np.max(y_val_true) + np.max(y_val_true)//6)
+    ax[0, 1].set_xlabel('Date')
+    ax[0, 1].set_ylabel('Passenger Count')
+
+    train_res = y_train_pred - y_train_true
+    ax[1, 0].plot(y_train_true.index, train_res, color='black')
+    ax[1, 0].set_title('Training Residuals')
+    ax[1, 0].set_xlim(y_train_true.index.start, y_train_true.index.stop)
+    ax[1, 0].set_ylim(np.min(train_res), np.max(train_res) + np.max(train_res)//6)
+    ax[1, 0].set_xlabel('Date')
+    ax[1, 0].set_ylabel('Passenger Count Residuals')
+    ax[1, 0].axhline(y=0, color='red') 
+
+    val_res = y_val_pred - y_val_true
+    ax[1, 1].plot(y_val_true.index, val_res, color='black')
+    ax[1, 1].set_title('Validation Residuals')
+    ax[1, 1].set_xlim(y_val_true.index.start, y_val_true.index.stop)
+    ax[1, 1].set_ylim(np.min(val_res), np.max(val_res) + np.max(val_res)//6)
+    ax[1, 1].set_xlabel('Date')
+    ax[1, 1].set_ylabel('Passenger Count Residuals')
+    ax[1, 1].axhline(y=0, color='red') 
 
     if dates:
         text_constant = 28
         
         dateInterval = len(y_train_true)//365*24
-        ax[0].set_xticks([i for i in range(y_train_true.index.start, y_train_true.index.stop, dateInterval)])
-        ax[0].set_xticklabels(trainDateCol[::dateInterval], rotation=90)
+        ax[0, 0].set_xticks([i for i in range(y_train_true.index.start, y_train_true.index.stop, dateInterval)])
+        ax[0, 0].set_xticklabels(trainDateCol[::dateInterval], rotation=90)
 
         dateInterval = len(y_val_true)//365*24
-        ax[1].set_xticks([i for i in range(y_val_true.index.start, y_val_true.index.stop, dateInterval)])
-        ax[1].set_xticklabels(valDateCol[::dateInterval], rotation=90)
+        ax[0, 1].set_xticks([i for i in range(y_val_true.index.start, y_val_true.index.stop, dateInterval)])
+        ax[0, 1].set_xticklabels(valDateCol[::dateInterval], rotation=90)
 
-    ax[0].text(y_train_true.index.start + (y_train_true.index.stop - y_train_true.index.start)//54, np.max(y_train_true) + np.max(y_train_true)//text_constant,
+        dateInterval = len(train_res)//365*24
+        ax[1, 0].set_xticks([i for i in range(y_train_true.index.start, y_train_true.index.stop, dateInterval)])
+        ax[1, 0].set_xticklabels(trainDateCol[::dateInterval], rotation=90)
+
+        dateInterval = len(val_res)//365*24
+        ax[1, 1].set_xticks([i for i in range(y_val_true.index.start, y_val_true.index.stop, dateInterval)])
+        ax[1, 1].set_xticklabels(valDateCol[::dateInterval], rotation=90)
+
+    ax[0, 0].text(y_train_true.index.start + (y_train_true.index.stop - y_train_true.index.start)//54, np.max(y_train_true) + np.max(y_train_true)//text_constant,
                f'Train MAE%: {MAE(y=y_train_true, yhat=y_train_pred)}\n'
                + f'Train MAPE: {MAPE(y=y_train_true, yhat=y_train_pred)}\n'
                + f'Train RMSE%: {RMSE(y=y_train_true, yhat=y_train_pred)}',
                bbox=dict(facecolor='white', alpha=0.5),
                fontsize=9)
-    ax[1].text(y_val_true.index.start + (y_val_true.index.stop - y_val_true.index.start)//54, np.max(y_val_true) + np.max(y_val_true)//text_constant,
+    ax[0, 1].text(y_val_true.index.start + (y_val_true.index.stop - y_val_true.index.start)//54, np.max(y_val_true) + np.max(y_val_true)//text_constant,
                f'Val MAE%: {MAE(y=y_val_true, yhat=y_val_pred)}\n'
                + f'Val MAPE: {MAPE(y=y_val_true, yhat=y_val_pred)}\n'
                + f'Val RMSE%: {RMSE(y=y_val_true, yhat=y_val_pred)}',
